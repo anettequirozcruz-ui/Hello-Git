601d6f1fe8bb09aa4fdb5c88e0b48af980ef381b
AnetteQC <anetteguadalupequirozCruz@outlook.com>
2025-10-20 19:32:59 -0500

    No se que estoy haciendo pero este es otro commit

--- Hellogit.py
+++ Hellogit.py
@@ -1,1 +1,7 @@
-Print ("Hola git")
+Print("¿Como te va mi amor?"
+      "como te va"
+      "Era en sielncio la pregunta entre tu yo"
+      "¿Eres feliz mi bien? sin engañar"
+      "Por que a mi puerta el amor nunca volvio"
+      "COMO TE VA MI AMORRRRRRRRRRRRRRR")
+
