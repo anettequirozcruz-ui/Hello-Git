3c8f5addec462964cfad9a902467167a5f7fc65b
anettep3 <anettequirozcruz@gmail.com>
2025-11-11 19:53:39 -0600

    se agrego el comit hola esto es la actividad 9 del tema 2

--- Hellogit.py
+++ Hellogit.py
@@ -5,3 +5,7 @@ Print("¿Como te va mi amor?"
       "Por que a mi puerta el amor nunca volvio"
       "COMO TE VA MI AMORRRRRRRRRRRRRRR")
 
+print("Hello esto es la actividad 9 del tema 2")
+print("Go ahead and cry, little girl Nobody does it like you do I know how much it matters to you I know that you got daddy issues")
+
+
